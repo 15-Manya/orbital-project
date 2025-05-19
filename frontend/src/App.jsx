3b0ecbb5271d4371cd21e3e997cdab349b8e9307
Manya <manya@1504>
2025-05-19 14:03:54 +0530

    Moved the background image from index.css to LandingPage.module.css

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,10 @@
 import './index.css';
-import Navbar from "./components/Navbar/Navbar";
+import LandingPage from './components/LandingPage/LandingPage';
 
 function App() {
   return (
     <div className="App">
-      <Navbar />
+      <LandingPage />
     </div>
   );
 }
---
+++ frontend/src/components/LandingPage/LandingPage.jsx
@@ -0,0 +1,11 @@
+import Navbar from '../Navbar/Navbar';
+import styles from './LandingPage.module.css';
+function LandingPage() {
+    return (
+        <div className={styles.body}>
+            <Navbar />
+        </div>
+    );
+}
+
+export default LandingPage;
