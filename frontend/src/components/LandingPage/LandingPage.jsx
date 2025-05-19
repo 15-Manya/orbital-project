985ad2c10fffefbb9e5429d9899ef31118d23baf
Manya <manya@1504>
2025-05-19 22:31:12 +0530

    Created the landing page

--- frontend/src/components/LandingPage/LandingPage.jsx
+++ frontend/src/components/LandingPage/LandingPage.jsx
@@ -4,6 +4,18 @@ function LandingPage() {
     return (
         <div className={styles.body}>
             <Navbar />
+            <div className={styles.landingPage}>
+                <div className='container-sm'>
+                    <div className="heading">
+                        <h1>Your next favorite book and podcast — recommended just for you.</h1>
+                        <p>Discover personalized reads and listens. One click away.</p>
+                    </div>
+                    <div className="enter-email">
+                        <input id = "email" type="email" placeholder='Enter your email'/>
+                        <button className={`btn ${styles.btndark}`}>Get Started</button>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
