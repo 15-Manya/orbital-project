cb02c7b32824c378761be9c6c2cf215247d873ff
Manya <manya@1504>
2025-05-22 22:31:12 +0530

    Setup Firebase.jsx

---
+++ frontend/react-app/src/Firebase.jsx
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBovKJXqhIFcTbrJEF2V6esEmn-7neRq1c",
+  authDomain: "nextbook-8e06a.firebaseapp.com",
+  projectId: "nextbook-8e06a",
+  storageBucket: "nextbook-8e06a.firebasestorage.app",
+  messagingSenderId: "24760970296",
+  appId: "1:24760970296:web:7e48480c01f8a50c514acd",
+  measurementId: "G-KLNF9F5CN5"
+};
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
