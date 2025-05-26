3f063635d8e0f23ead57845789a5b295154e7476
ShamitGupta <e1459301@u.nus.edu>
2025-05-27 00:14:06 +0800

    Added Personalize Experience page

--- frontend/react-app/src/App.jsx
+++ frontend/react-app/src/App.jsx
@@ -2,6 +2,7 @@ import './index.css';
 import LandingPage from './components/LandingPage/LandingPage';
 import Login from './components/Login/Login';
 import SignUp from './components/SignUp/SignUp'
+import PersonalizeExperience from './components/PersonalizeExperience/PersonalizeExperience';
 import {BrowserRouter, Routes, Route} from 'react-router';
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
         <Route path='/' element ={<LandingPage />}/>
         <Route path='/login' element ={<Login />} /> 
         <Route path='/signup' element = {<SignUp/>}/>
+        <Route path='/experience' element = {<PersonalizeExperience/>}/> 
 
       </Routes>
     </BrowserRouter>
