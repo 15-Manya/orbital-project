d50660a445911cc8be07141af4acb32ed5a3dd02
Manya <manya@1504>
2025-05-31 13:41:38 +0530

    Modified the css for Login Page

--- frontend/react-app/src/components/Login/Login.jsx
+++ frontend/react-app/src/components/Login/Login.jsx
@@ -52,39 +52,42 @@ function Login() {
     return (
         <>
             <div className={styles.body}>
+                <div className={`container ${styles.display}`}>
+                    <div className={styles.white_box}>
+                        <Link to="/library"><img className={styles.logo} src={logo}></img></Link>
 
-                <div className={styles.white_box}>
-                    <Link to="/library"><img className={styles.logo} src={logo}></img></Link>
+                        <p className={styles.login}><b>Login to your account</b></p>
+                        <div className={styles.credentials}>
+                            <div className={styles.username}>
+                                {error && <p className={styles.error}>{error}</p>}
+                                <p className={styles.email}><b>Email or Username</b></p>
+                                <input type="text" placeholder="Email or Username" className={styles.box1} onChange={e => setEmail(e.target.value)}/>
 
-                    <p className={styles.login}><b>Login to your account</b></p>
+                                <p className={styles.password}><b>Password</b></p>
+                                <input type="password" placeholder="Password" className={styles.box2} onChange={e => setPassword(e.target.value)}/>
+                            </div>
+                        </div>
 
-                    <div className={styles.username}>
-                        {error && <p className={styles.error}>{error}</p>}
-                        <p className={styles.email}><b>Email or Username</b></p>
-                        <input type="text" placeholder="Email or Username" className={styles.box1} onChange={e => setEmail(e.target.value)}/>
+                        <Link to="/forgot-password" className={styles.forgot_password}>forgot password?</Link>
 
-                        <p className={styles.password}><b>Password</b></p>
-                        <input type="password" placeholder="Password" className={styles.box2} onChange={e => setPassword(e.target.value)}/>
-                    </div>
+                        <div className={styles.remember_me}>
+                            <input type="checkbox" className={styles.select}/>Remember Me
+                        </div>
 
-                    <Link to="/forgot-password" className={styles.forgot_password}>forgot password?</Link>
+                        <button className={`btn ${styles.btndark}`} onClick={handleLogin}>Login</button>
 
-                    <div className={styles.remember_me}>
-                        <input type="checkbox" className={styles.select}/>Remember Me
-                    </div>
+                        <p className={styles.or}>or</p>
 
-                    <button className={styles.login_button} onClick={handleLogin}>Login</button>
-
-                    <p className={styles.or}>or</p>
-
-                    <button className={styles.google} onClick={signInWithGoogle}>
-                        <img src={google} className={styles.icon}></img>
-                        Continue with Google
-                    </button>
-
-                    <div className={styles.no_account}>
-                        Don't have an account?
-                        <Link to='/signup'>Sign up</Link>
+                        <button className={`btn ${styles.google}`} onClick={signInWithGoogle}>
+                            <img src={google} className={styles.icon}></img>
+                            Continue with Google
+                        </button>
+                        <div className={styles.sign_up}>
+                            <p>
+                                Don't have an account?
+                                <Link to="/signup"> Sign up</Link>
+                            </p>
+                        </div>
                     </div>
                 </div>
             </div>
