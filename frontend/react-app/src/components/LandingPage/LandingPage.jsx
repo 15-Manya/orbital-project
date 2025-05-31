4f6c40baaf142e0006d63d5ee9f92794e689662a
Manya <manya@1504>
2025-05-31 16:00:24 +0530

    Linked the Get Started to the signup page

--- frontend/react-app/src/components/LandingPage/LandingPage.jsx
+++ frontend/react-app/src/components/LandingPage/LandingPage.jsx
@@ -1,6 +1,11 @@
 import Navbar from '../Navbar/Navbar';
 import styles from './LandingPage.module.css';
+import { useState } from 'react';
+import { Link } from 'react-router';
+
 function LandingPage() {
+    const [email, setEmail] = useState('');
+
     return (
         <div className={styles.body}>
             <Navbar />
@@ -10,9 +15,11 @@ function LandingPage() {
                         <h1 className={styles.main_heading}>Your next favorite book and podcast — recommended just for you.</h1>
                         <p className={styles.sub_heading}>Discover personalized reads and listens. One click away.</p>
                     </div>
-                    <div className="enter-email">
-                        <input id = "email" type="email" placeholder='Enter your email'/>
-                        <button className={`btn ${styles.btndark}`}>Get Started</button>
+                    <div className={styles.enter}>
+                        <input id = "email" type="email" placeholder='Enter your email' required value={email} onChange={(e) => setEmail(e.target.value)}/>
+                        <Link to="/signup">
+                            <button className={`btn ${styles.btndark}`}>Get Started</button>
+                        </Link>
                     </div>
                 </div>
             </div>
