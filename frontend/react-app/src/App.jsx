5b787bf3c973a6fc7e538896840212d6a12bb53f
Manya <manya@1504>
2025-05-31 14:24:54 +0530

    Modified css for forgot password

--- frontend/react-app/src/App.jsx
+++ frontend/react-app/src/App.jsx
@@ -7,7 +7,7 @@ import Question2 from './components/Questions/Question2/Question2';
 import Question3 from './components/Questions/Question3/Question3';
 import PersonalizeExperience from './components/Questions/PersonalizeExperience/PersonalizeExperience';
 import PersonalizeLibrary from './components/Questions/PersonalizeLibrary/PersonalizeLibrary';
-import ForgotPassWord from './components/ForgotPassWord/ForgotPassWord';
+import ForgotPassword from './components/Login/ForgotPassWord/ForgotPassWord';
 import {BrowserRouter, Routes, Route} from 'react-router';
 
 function App() {
@@ -22,7 +22,7 @@ function App() {
         <Route path="/q2" element={<Question2/>}/>
         <Route path='/q3' element={<Question3/>}/>
         <Route path="/personalizeLibrary" element={<PersonalizeLibrary/>}/>
-        <Route path="/forgot-password" element={<ForgotPassWord />} />
+        <Route path="/forgot-password" element={<ForgotPassword />} />
       </Routes>
     </BrowserRouter>
   );
