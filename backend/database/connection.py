5832fdf9cda1aca166aa56693e3ede9973efa952
Manya <manya@1504>
2025-06-15 13:42:09 +0530

    Created the user model and user collection

--- backend/database/connection.py
+++ backend/database/connection.py
@@ -4,3 +4,8 @@ from pymongo.server_api import ServerApi
 #Setting up the database
 MONGO_URL = "mongodb+srv://techTitans:techTitansGo@nextbook.xui6btl.mongodb.net/?retryWrites=true&w=majority&appName=NextBook"
 client = MongoClient(MONGO_URL, server_api=ServerApi('1')) # Create a new client and connect to the server
+db = client['NextBook']
+user_collection = db['users']
+
+#user_collection.insert_one({"username": "manya"})
+#print(user_collection.find_one({"username": "manya"}))
